d6664144f93c2c0a5997f81c20b157b723ac6b16
Cam Beaudoin <camilebeaudoin@gmail.com>
2020-11-22 23:09:12 -0500

    cleanup

--- my-docz-app/doczrc.js
+++ my-docz-app/doczrc.js
@@ -1,6 +1,6 @@
 export default {
     title: 'A11y Code',
-    description: '100% purely accessible components... finally',
+    description: '100% purely accessible components... Finally',
     src: './src',
     files: '**/*.{md,mdx}',
     menu: ['Getting Started', 'Simple Components', 'Complex Components', 'Structural Components', 'Design']
